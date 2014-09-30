76ddfdd3c3f6d15652cf31e7b2281d024132b0c2
Kenichi Fukuda <kenichi.fukuda07@gmail.com>
2014-09-30 20:20:05 +0900

    brewfile.sh added

---
+++ brewfile.sh
@@ -0,0 +1,45 @@
+#!/bin/sh# update Homebrew
+brew update
+
+# Update formula
+brew upgrade
+
+# Add Repository
+brew tap phinze/homebrew-cask
+brew tap sanemat/font
+brew tap homebrew/science
+brew tap caskroom/fonts
+
+# build or install
+brew install git
+brew install tig
+brew install openssl
+brew install wget
+brew install ruby
+brew install zsh
+brew install ricty
+brew install brew-cask
+brew install cscope
+brew install lua
+brew install luajit
+brew install scala
+brew linkapps
+
+# cask install
+brew cask install java
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install firefox
+brew cask install evernote
+brew cask install skype
+brew cask install kobito
+brew cask install virtualbox
+brew cask install vlc
+brew cask install dropbox
+brew cask install sublime-text
+brew cask install mi
+brew cask install iterm2
+brew cask install sequel-pro
+brew cask install adobe-reader
+brew cask install imageoptim
+brew cask install keka
